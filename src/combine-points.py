e4a5e68fb9421073ec1b2978c791a5536427cf1c
Damien Snyder <signup@damiensnyder.com>
2022-10-30 17:03:51 -0700

    preserve string matchid

--- src/combine-points.py
+++ src/combine-points.py
@@ -8,11 +8,11 @@ for year in range(11):
     print(current_year)
     filename = f"{current_year}-combined-points.csv"
 
-    ausopen = pd.read_csv(f"../data/grand-slam-point-data/{current_year}-ausopen-points.csv", low_memory=False)
-    frenchopen = pd.read_csv(f"../data/grand-slam-point-data/{current_year}-frenchopen-points.csv", low_memory=False)
-    usopen = pd.read_csv(f"../data/grand-slam-point-data/{current_year}-usopen-points.csv", low_memory=False)
+    ausopen = pd.read_csv(f"data/grand-slam-point-data/{current_year}-ausopen-points.csv", low_memory=False)
+    frenchopen = pd.read_csv(f"data/grand-slam-point-data/{current_year}-frenchopen-points.csv", low_memory=False)
+    usopen = pd.read_csv(f"data/grand-slam-point-data/{current_year}-usopen-points.csv", low_memory=False)
     if current_year != 2020:
-        wimbledon = pd.read_csv(f"../data/grand-slam-point-data/{current_year}-wimbledon-points.csv", low_memory=False)
+        wimbledon = pd.read_csv(f"data/grand-slam-point-data/{current_year}-wimbledon-points.csv", low_memory=False)
 
     grand_slams = [ausopen, frenchopen, usopen, wimbledon]
     result = pd.concat(grand_slams, axis=0)
@@ -20,6 +20,5 @@ for year in range(11):
     # Filter only men's singles matches
     result['match_id_num'] = result['match_id'].str.split('-').str[2]
     result['match_id_num'] = pd.to_numeric(result['match_id_num'], errors='coerce')
-    result.drop(columns=result.columns[0], inplace=True, axis=1)
     result = result[result['match_id_num'] < 2000]
-    result.to_csv(f"../data/grand-slam-point-data/combined-points/{filename}")
+    result.to_csv(f"data/grand-slam-point-data/combined-points/{filename}")
